a7bf24eaff3b363acc4f63fb12d5f4f0962be741
chandan3966 <chandan3966.official@gmail.com>
2019-10-06 01:07:33 +0530

    till last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'panda';
+  tabs = ['Blog/Article','Newsletter/E-mailer','Whitepaper','E-book','Report/Guide','Product Description','Website Content','Video Script','Company Profile/Brochure','Press Release']
+  subtabs = ['from Rs.500','from Rs.1000','from Rs.2000','from Rs.1000','from Rs.1000','from Rs.500','from Rs.1000','from Rs.1000','from Rs.2000','from Rs.2000']
 }
